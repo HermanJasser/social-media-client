a54052f2632ff008046f9774be7bcd5160980262
HermanJasser <herjas00989@stud.noroff.no>
2024-10-18 20:51:19 +0200

    fått cypress til å rune nå må jeg bare få scriptet riktig

--- cypress/e2e/logInLogOut.cy.js
+++ cypress/e2e/logInLogOut.cy.js
@@ -1,9 +1,16 @@
 describe('Login Test', () => {
   it('should allow a user to log in with valid credentials', () => {
     cy.visit('http://127.0.0.1:8080/')
-    cy.get('input[name="email"]').type('heidu@stud.noroff.no') // Sett inn gyldig e-post
-    cy.get('input[name="password"]').type('heidu123') // Sett inn gyldig passord
-    cy.get('form').submit() // Send skjemaet
+    cy.wait(2000)
+
+    cy.get('[data-auth="login"]').first().click()
+    cy.wait(2000)
+    cy.get('input[name="email"]').type('heidu@stud.noroff.no')
+    cy.wait(2000)
+    cy.get('input[name="password"]').type('heidu123') //
+    cy.wait(2000)
+    cy.get('form').submit()
+    cy.wait(2000)
 
     // Sjekk om omdirigeringen til profil-siden skjer etter vellykket innlogging
     cy.url().should('include', '/?view=profile')
