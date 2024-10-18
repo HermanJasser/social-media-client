34243dff0c19af5dbbae2accb68f2596c2d51cc8
HermanJasser <herjas00989@stud.noroff.no>
2024-10-18 21:31:25 +0200

    da funker alle testen i cypress

--- cypress/e2e/logInLogOut.cy.js
+++ cypress/e2e/logInLogOut.cy.js
@@ -3,16 +3,62 @@ describe('Login Test', () => {
     cy.visit('http://127.0.0.1:8080/')
     cy.wait(2000)
 
-    cy.get('[data-auth="login"]').first().click()
+    cy.get('[data-auth="login"]').last().click()
     cy.wait(2000)
-    cy.get('input[name="email"]').type('heidu@stud.noroff.no')
+    cy.get('#loginEmail').type('heidu@stud.noroff.no')
     cy.wait(2000)
-    cy.get('input[name="password"]').type('heidu123') //
+    cy.get('#loginPassword').type('heidu123')
     cy.wait(2000)
-    cy.get('form').submit()
+    cy.get('#loginBtn').click()
     cy.wait(2000)
 
-    // Sjekk om omdirigeringen til profil-siden skjer etter vellykket innlogging
     cy.url().should('include', '/?view=profile')
+  })
+})
+
+describe(' invalid login Test', () => {
+  it('should not allow a user to log in with invalid credentials', () => {
+    cy.visit('http://127.0.0.1:8080/')
+    cy.wait(2000)
+
+    cy.get('[data-auth="login"]').last().click()
+    cy.wait(2000)
+    cy.get('#loginEmail').type('feilja@stud.noroff.no')
+    cy.wait(2000)
+    cy.get('#loginPassword').type('feilja123')
+    cy.wait(2000)
+    cy.get('#loginBtn').click()
+    cy.wait(2000)
+
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains(
+        'Either your username was not found or your password is incorrect'
+      )
+    })
+  })
+})
+
+describe('Log out Test', () => {
+  it('should allow the user to log out', () => {
+    cy.visit('http://127.0.0.1:8080/')
+    cy.wait(2000)
+
+    cy.get('[data-auth="login"]').last().click()
+    cy.wait(2000)
+    cy.get('#loginEmail').type('heidu@stud.noroff.no')
+    cy.wait(2000)
+    cy.get('#loginPassword').type('heidu123')
+    cy.wait(2000)
+    cy.get('#loginBtn').click()
+    cy.wait(2000)
+
+    cy.get('[data-auth="logout"]').click()
+
+    cy.window().then((window) => {
+      const token = window.localStorage.getItem('token') // 'key' er navnet på verdien i localStorage
+      const profile = window.localStorage.getItem('profile') // 'key' er navnet på verdien i localStorage
+      expect(token).to.be.null
+      expect(profile).to.be.null
+    })
   })
 })
